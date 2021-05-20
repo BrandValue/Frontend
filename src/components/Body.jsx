88d101bf6555eb8d60f9d9812b911ec005134858
Swaraj Deep <deep.swaraj7@gmail.com>
2021-05-21 00:56:48 +0530

    Add first row of body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import BackGround from "../assets/backgroundImages/background.jpg"
-import {Container, Grid, Paper} from "@material-ui/core";
+import {Box, IconButton, OutlinedInput, Paper} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
+import SearchIcon from '@material-ui/icons/Search';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -9,35 +10,79 @@ const useStyles = makeStyles((theme) => ({
         padding: '0.5rem'
     },
     paper: {
-        padding: theme.spacing(2),
         textAlign: 'center',
         color: theme.palette.text.secondary,
     },
-    paperContainer1: {
+    paperContainer: {
         backgroundImage: `url(${BackGround})`,
         height: "500px",
         width: "500px",
-        backgroundRepeat: 'no-repeat'
+        backgroundRepeat: 'no-repeat',
+        display: "flex",
+        [theme.breakpoints.down('sm')]: {
+            display: "none",
+        },
+    },
+    textGroup: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: "230px",
+        paddingLeft: "15px",
+        paddingRight: "15px",
+        [theme.breakpoints.down('sm')]: {
+            paddingTop: "5px",
+        },
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        fontFamily: "monospace",
+        fontSize: 18,
+        paddingLeft: `calc(1em + ${theme.spacing(2)}px)`,
+        transition: theme.transitions.create('width'),
+        height: "35px",
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        [theme.breakpoints.up('md')]: {
+            width: '45ch',
+            '&:focus': {
+                width: '55ch',
+            },
+        },
+        [theme.breakpoints.down('md')]: {
+            width: '15ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
     },
 }));
 
-function Body(props) {
+function Body() {
     const classes = useStyles();
     return (
-        <div className={`${classes.root} container`}>
-            <Grid container
-                  direction="row"
-                  justify="center"
-                  alignItems="center">
-                <Grid item xs={12} sm={12} md={6} lg={7}>
-                    ...
-                </Grid>
-                <Grid item xs={12} sm={12} md={6} lg={5} className={'d-none d-lg-block'}>
-                    <Paper className={classes.paperContainer1}/>
-                </Grid>
-            </Grid>
+        <div className={`${classes.root}`}>
+            <Box display="flex">
+                <Box flexGrow={1} className={classes.textGroup}>
+                    <OutlinedInput
+                        placeholder="Start eating…"
+                        classes={{
+                            root: classes.inputRoot,
+                            input: classes.inputInput,
+                        }}
+                        inputProps={{'aria-label': 'search'}}
+                        endAdornment={<IconButton position="end"><SearchIcon/></IconButton>}
+                    />
+                </Box>
+                <Box className={classes.paper}>
+                    <Paper className={classes.paperContainer}/>
+                </Box>
+            </Box>
         </div>
     );
 }
+
 
 export default Body;
